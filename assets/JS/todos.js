ffc8104e9f858c9e07135756785abdc89b11b967
Isaac Watt <isaacwatt@icloud.com>
2018-06-05 19:17:48 -0400

    allows user to cross off items

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,1 +1,14 @@
-alert("hello");
+$(document).ready(function() {
+  // finished task
+  $("li").click(function(){
+    $(this).toggleClass("completed");
+  });
+
+  // delete task
+  $("span").click(function(event) {
+    $(this).parent().fadeOut("slow", function() {
+      $(this).parent().remove();
+    })
+    event.stopPropagation(); // stops bubbling
+  })
+});
