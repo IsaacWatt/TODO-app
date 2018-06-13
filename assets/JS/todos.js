ba5e1f81db11eaa62af3f1bb416d700fdf02032f
Isaac Watt <isaacwatt@icloud.com>
2018-06-12 21:06:43 -0400

    implements dynamic todos

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
   // finished task
-  $("li").click(function(){
+  $("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
   });
 
   // delete task
-  $("span").click(function(event) {
+  $("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut("slow", function() {
-      $(this).parent().remove();
+      $(this).remove();
     });
     event.stopPropagation(); // stops bubbling
   })
+
+  $("input[type='text']").keypress( function(event) {
+    if (event.which === 13) { // enter key
+      var toDo = $(this).val();
+      $(this).val("");
+      var spanTag = "<span>X </span>"
+
+      $("ul").append("<li> " + spanTag + toDo +"</li>")
+    }
+  });
+
 });
